7072b3b6b5be8ae7a2aa266a602590e76dfc3bd5
miguelq89 <miguelhquinteros@gmail.com>
2021-04-25 22:08:16 -0300

    primer version login

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ app.use(express.static(publicPath));
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
-
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/views/productCart.html'));
+});
+app.get('/login', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
 });
