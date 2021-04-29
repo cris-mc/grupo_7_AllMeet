02a736c69eca9e407331f1949f4b0934ff59b478
miguelq89 <miguelhquinteros@gmail.com>
2021-04-29 16:22:58 -0300

    actualizacion productdetail.html

--- app.js
+++ app.js
@@ -18,3 +18,6 @@ app.get('/productCart', (req, res)=> {
 app.get('/login', (req, res)=> {
     res.sendFile(path.join(__dirname, '/views/login.html'));
 });
+app.get('/productdetail', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'));
+});
